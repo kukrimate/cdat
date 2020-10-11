44c21cdc8c40062cfa325b25319fb5561336cc1c
Mate Kukri <kukri.mate@gmail.com>
2020-10-11 13:23:09 +0100

    htab: Use open-addressing instead of chaining

--- htab.h
+++ htab.h
@@ -1,7 +1,6 @@
 /*
- * Chained hash table
+ * Generic hash table
  * Copyright (C) Mate Kurki, 2020
- * Part of libkm, released under the ISC license
  */
 #ifndef HTAB_H
 #define HTAB_H
@@ -29,99 +28,66 @@
 struct helem##A { \
 	T key; \
 	U value; \
-	struct helem##A *next; \
 }; \
 \
 struct htab##A { \
-	size_t filled_cnt; \
-	size_t bucket_cnt; \
-	struct helem##A **buckets; \
+	size_t load; \
+	size_t size; \
+	struct helem##A *arr; \
 }; \
 \
 static inline void htab_alloc##A(struct htab##A *self) \
 { \
-	self->filled_cnt = 0; \
-	self->bucket_cnt = HTAB_START_BUCKETS; \
-	self->buckets = calloc(self->bucket_cnt, sizeof(struct helem##A *)); \
+	self->load = 0; \
+	self->size = HTAB_START_BUCKETS; \
+	self->arr = calloc(self->size, sizeof(struct helem##A)); \
 } \
 \
 static inline void htab_free##A(struct htab##A *self) \
 { \
-	struct helem##A **cur, *node, *tmp; \
-\
-	for (cur = self->buckets; cur < self->buckets + self->bucket_cnt; ++cur) \
-		for (node = *cur; node; tmp = node->next, free(node), node = tmp); \
-\
-	free(self->buckets); \
-} \
-static inline void htab_putuniq##A(struct htab##A *self, T key, U val) \
-{ \
-	struct helem##A **ptr; \
-\
-	ptr = self->buckets + hash(key) % self->bucket_cnt; \
-\
-	if (!*ptr) \
-		++self->filled_cnt; \
-	else \
-		for (; *ptr; ptr = &(*ptr)->next); \
-\
-	*ptr = malloc(sizeof(struct helem##A)); \
-	(*ptr)->key = key; \
-	(*ptr)->value = val; \
-	(*ptr)->next = NULL; \
-} \
-\
-static inline void htab_grow##A(struct htab##A *self) \
-{ \
-	struct helem##A **old, **cur, **end, *node, *tmp; \
-\
-	cur = old = self->buckets; \
-	end = cur + self->bucket_cnt; \
-\
-	self->filled_cnt = 0; \
-	self->bucket_cnt *= HTAB_GROW_FACTOR; \
-	self->buckets = calloc(self->bucket_cnt, sizeof(struct helem *)); \
-\
-	for (; cur < end; ++cur) \
-		for (node = *cur; node; tmp = node->next, free(node), node = tmp) \
-			htab_putuniq##A(self, node->key, node->value); \
-\
-	free(old); \
+	free(self->arr); \
 } \
 \
 static inline void htab_put##A(struct htab##A *self, T key, U val) \
 { \
-	struct helem##A **ptr; \
+	size_t i; \
 \
-	if (self->filled_cnt > HTAB_LOAD_LIMIT(self->bucket_cnt)) \
-		htab_grow##A(self); \
+	size_t oldsize; \
+	struct helem##A *oldarr; \
 \
-	ptr = self->buckets + hash(key) % self->bucket_cnt; \
+	for (i = hash(key) % self->size; \
+			self->arr[i].key; \
+			i = (i + 1) % self->size) \
+		if (cmp(self->arr[i].key, key)) \
+			break; \
 \
-	if (!*ptr) \
-		++self->filled_cnt; \
-	else \
-		for (; *ptr; ptr = &(*ptr)->next) \
-			if (cmp((*ptr)->key, key)) \
-				goto insert; \
+	self->arr[i].key = key; \
+	self->arr[i].value = val; \
 \
-	*ptr = malloc(sizeof(struct helem##A)); \
-	(*ptr)->next = NULL; \
-insert: \
-	(*ptr)->key = key; \
-	(*ptr)->value = val; \
+	if (++self->load >= HTAB_LOAD_LIMIT(self->size)) { \
+		oldsize = self->size; \
+		oldarr = self->arr; \
+\
+		self->size *= HTAB_GROW_FACTOR; \
+		self->arr = calloc(self->size, sizeof(struct helem##A)); \
+\
+		for (i = 0; i < oldsize; ++i) \
+			if (oldarr[i].key) \
+				htab_put##A(self, \
+					oldarr[i].key, oldarr[i].value); \
+\
+		free(oldarr); \
+	} \
 } \
 \
 static inline U htab_get##A(struct htab##A *self, T key) \
 { \
-	struct helem##A **ptr; \
-\
-	ptr = self->buckets + hash(key) % self->bucket_cnt; \
-\
-	for (; *ptr; ptr = &(*ptr)->next) \
-		if (cmp((*ptr)->key, key)) \
-			return (*ptr)->value; \
-\
+	size_t i; \
+	for (i = hash(key) % self->size; \
+			self->arr[i].key; \
+			i = (i + 1) % self->size) \
+		if (cmp(self->arr[i].key, key)) \
+			return self->arr[i].value; \
 	return NULL; \
 }
 
@@ -151,6 +117,5 @@ static inline int str_cmp(char *str1, char *str2)
 {
 	return !strcmp(str1, str2);
 }
-
 
 #endif
