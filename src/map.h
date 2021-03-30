e1284c6c6cf6997f404248742dfcb60fa80b67dd
Mate Kukri <kukri.mate@gmail.com>
2021-03-30 17:47:04 +0100

    Map: implement a slightly better hash-table

--- src/map.h
+++ src/map.h
@@ -5,156 +5,139 @@
 #ifndef MAP_H
 #define MAP_H
 
-/*
- * Number of preallocated buckets
- */
-#ifndef MAP_PREALLOC
-#define MAP_PREALLOC 10
+/* Minimum number of buckets */
+#ifndef MAP_MINSIZE
+#define MAP_MINSIZE 8
 #endif
 
-/*
- * Rehash the table if the load is over this number
- */
-#ifndef MAP_REHASH_TRESHOLD
-#define MAP_REHASH_TRESHOLD(size) (size * 3 / 4)
+/* Maximum number of used buckets allowed for a given table */
+#ifndef MAP_RESIZE_TRESHOLD
+#define MAP_RESIZE_TRESHOLD(size) (size / 2)
 #endif
 
-/*
- * Grow the table if the load would be over this number after a rehash
- */
-#ifndef MAP_GROW_TRESHOLD
-#define MAP_GROW_TRESHOLD(size) (size / 2)
+/* The new table size will be round_pow2(self->used * MAP_RESIZE_FACTOR) */
+#ifndef MAP_RESIZE_FACTOR
+#define MAP_RESIZE_FACTOR 3
 #endif
 
-/*
- * Multiplier for the size when a grow is triggered
- */
-#ifndef MAP_GROW_FACTOR
-#define MAP_GROW_FACTOR 2
-#endif
+/* Round to the nearest power of 2 >= to size */
+static inline size_t round_pow2(size_t size)
+{
+    size_t newsize = MAP_MINSIZE;
+    while (newsize < size)
+        newsize <<= 1;
+    return newsize;
+}
+
+/* State of a bucket */
+enum {
+    BS_EMPTY,  // Empty
+    BS_DUMMY,  // Previously active
+    BS_ACTIVE, // Currently active
+};
 
 /*
  * Generate type specific definitions
  */
-#define MAP_GEN(ktype, vtype, khash, kcmp, alias) \
-\
-typedef struct { \
-	_Bool present; \
-	_Bool deleted; \
-	ktype key; \
-	vtype val; \
-} MAP_ELEM##alias; \
-\
-typedef struct { \
-	size_t present_cnt; \
-	size_t deleted_cnt; \
-	size_t size; \
-	MAP_ELEM##alias *arr; \
-} MAP##alias; \
-\
-static inline void MAP##alias##_init(MAP##alias *self) \
-{ \
-	self->present_cnt = 0; \
-	self->deleted_cnt = 0; \
-	self->size = MAP_PREALLOC; \
-	self->arr = calloc(self->size, sizeof(*self->arr)); \
-} \
-\
-static inline void MAP##alias##_free(MAP##alias *self) \
-{ \
-	free(self->arr); \
-} \
-\
-static inline vtype *MAP##alias##_put(MAP##alias *self, ktype key); \
-\
-static inline void MAP##alias##_rehash(MAP##alias *self) \
-{ \
-	size_t i, oldsize; \
-	MAP_ELEM##alias *oldarr; \
-\
-	/* Save old bucket array for rehashing */ \
-	oldsize = self->size; \
-	oldarr = self->arr; \
-\
-	/* Check if removing deleted pairs is enough or do we need to grow */ \
-	if (self->present_cnt - self->deleted_cnt > MAP_GROW_TRESHOLD(self->size)) \
-		self->size *= MAP_GROW_FACTOR; \
-	/* Set deleted and present count to 0 initially for the new table */ \
-	self->present_cnt = 0; \
-	self->deleted_cnt = 0; \
-	/* Allocate new bucket array */ \
-	self->arr = calloc(self->size, sizeof(*self->arr)); \
-\
-	/* Put all non-deleted pairs into the new table */ \
-	for (i = 0; i < oldsize; ++i) \
-		if (oldarr[i].present && !oldarr[i].deleted)\
-			*MAP##alias##_put(self, oldarr[i].key) = oldarr[i].val; \
-\
-	free(oldarr); \
-} \
-\
-static inline vtype *MAP##alias##_put(MAP##alias *self, ktype key) \
-{ \
-	size_t i; \
-	\
-	/* Check if a rehash is needed before inserting a new pair */ \
-	if (self->present_cnt > MAP_REHASH_TRESHOLD(self->size)) \
-		MAP##alias##_rehash(self); \
-\
-	/* Probe for the key (even if deleted) or an empty bucket */ \
-	for (i = khash(key) % self->size; \
-			self->arr[i].present; i = (i + 1) % self->size) \
-		if (kcmp(self->arr[i].key, key)) \
-			break; \
-\
-	/* Increase load if the key was not present in the table before */ \
-	if (!self->arr[i].present) \
-		++self->present_cnt; \
-	/* Decrease deleted count if re-filling a deleted pair's bucket */ \
-	else if (self->arr[i].deleted) \
-		--self->deleted_cnt; \
-\
-	/* Fill bucket with the pair */ \
-	self->arr[i].present = 1; \
-	self->arr[i].deleted = 0; \
-	self->arr[i].key = key; \
-	return &self->arr[i].val; \
-} \
-\
-static inline vtype *MAP##alias##_get(MAP##alias *self, ktype key) \
-{ \
-	size_t i; \
-\
-	for (i = khash(key) % self->size; \
-			self->arr[i].present; i = (i + 1) % self->size) \
-		if (kcmp(self->arr[i].key, key)) { \
-			/* Signal not-present if the pair was deleted */ \
-			if (self->arr[i].deleted) \
-				return NULL; \
-			/* Return pointer to value */ \
-			return &self->arr[i].val; \
-		} \
-	/* Signal not-present if key was found */ \
-	return NULL; \
-} \
-\
-static inline void MAP##alias##_del(MAP##alias *self, ktype key) \
-{ \
-	size_t i; \
-\
-	for (i = khash(key) % self->size; \
-			self->arr[i].present; i = (i + 1) % self->size) \
-		if (kcmp(self->arr[i].key, key)) { \
-			/* Avoid double counting if the pair was already deleted */ \
-			if (self->arr[i].deleted) \
-				return; \
-			/* Mark pair as deleted if it wasn't already marked */ \
-			self->arr[i].deleted = 1; \
-			++self->deleted_cnt; \
-			return; \
-		} \
-}
-
+#define MAP_GEN(ktype, vtype, khash, kcmp, alias)                              \
+                                                                               \
+typedef struct {                                                               \
+    unsigned char state;                                                       \
+    ktype key;                                                                 \
+    vtype val;                                                                 \
+} MAP_ELEM##alias;                                                             \
+                                                                               \
+typedef struct {                                                               \
+    size_t active_cnt;                                                         \
+    size_t size;                                                               \
+    MAP_ELEM##alias *arr;                                                      \
+} MAP##alias;                                                                  \
+                                                                               \
+static inline void MAP##alias##_init(MAP##alias *self)                         \
+{                                                                              \
+    self->active_cnt = 0;                                                      \
+    self->size = MAP_MINSIZE;                                                  \
+    self->arr = calloc(self->size, sizeof *self->arr);                         \
+}                                                                              \
+                                                                               \
+static inline void MAP##alias##_free(MAP##alias *self)                         \
+{                                                                              \
+    free(self->arr);                                                           \
+}                                                                              \
+                                                                               \
+static inline vtype *MAP##alias##_put(MAP##alias *self, ktype key);            \
+                                                                               \
+static inline void MAP##alias##_resize(MAP##alias *self)                       \
+{                                                                              \
+    size_t oldsize = self->size;                                               \
+    MAP_ELEM##alias *oldarr = self->arr;                                       \
+                                                                               \
+    self->size = round_pow2(self->active_cnt * MAP_RESIZE_FACTOR);             \
+    self->active_cnt = 0;                                                      \
+    self->arr = calloc(self->size, sizeof *self->arr);                         \
+                                                                               \
+    for (size_t i = 0; i < oldsize; ++i)                                       \
+        if (oldarr[i].state == BS_ACTIVE) {                                    \
+            /* Put contents of active bucket into the new table */             \
+            *MAP##alias##_put(self, oldarr[i].key) = oldarr[i].val;            \
+        }                                                                      \
+                                                                               \
+    free(oldarr);                                                              \
+}                                                                              \
+                                                                               \
+static inline vtype *MAP##alias##_put(MAP##alias *self, ktype key)             \
+{                                                                              \
+    if (self->active_cnt >= MAP_RESIZE_TRESHOLD(self->size))                   \
+        MAP##alias##_resize(self);                                             \
+                                                                               \
+    size_t i = khash(key) % self->size;                                        \
+                                                                               \
+    for (;;) {                                                                 \
+        if (self->arr[i].state != BS_ACTIVE) {                                 \
+            /* Filling non-active bucket */                                    \
+            ++self->active_cnt;                                                \
+            break;                                                             \
+        }                                                                      \
+        if (kcmp(self->arr[i].key, key)) {                                     \
+            /* Modifying active bucket with a matching key */                  \
+            break;                                                             \
+        }                                                                      \
+        i = (i + 1) % self->size;                                              \
+    }                                                                          \
+                                                                               \
+    self->arr[i].state = BS_ACTIVE;                                            \
+    self->arr[i].key = key;                                                    \
+    return &self->arr[i].val;                                                  \
+}                                                                              \
+                                                                               \
+static inline vtype *MAP##alias##_get(MAP##alias *self, ktype key)             \
+{                                                                              \
+    size_t i = khash(key) % self->size;                                        \
+                                                                               \
+    while (self->arr[i].state != BS_EMPTY) {                                   \
+        if (self->arr[i].state == BS_ACTIVE && kcmp(self->arr[i].key, key)) {  \
+            /* Key of active bucket matches */                                 \
+            return &self->arr[i].val;                                          \
+        }                                                                      \
+        i = (i + 1) % self->size;                                              \
+    }                                                                          \
+    return NULL;                                                               \
+}                                                                              \
+                                                                               \
+static inline void MAP##alias##_del(MAP##alias *self, ktype key)               \
+{                                                                              \
+    size_t i = khash(key) % self->size;                                        \
+                                                                               \
+    while (self->arr[i].state != BS_EMPTY) {                                   \
+        if (self->arr[i].state == BS_ACTIVE && kcmp(self->arr[i].key, key)) {  \
+            /* Key of active bucket matches */                                 \
+            self->arr[i].state = BS_DUMMY;                                     \
+            --self->active_cnt;                                                \
+            return;                                                            \
+        }                                                                      \
+        i = (i + 1) % self->size;                                              \
+    }                                                                          \
+}                                                                              \
 
 /*
  * Hash and compare for scaler values
